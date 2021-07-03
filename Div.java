829f63e3dd41e0e4eccf01bc80a59bd77857dac4
faiyaz <misterfaiyaz123jio@gmail.com>
2021-07-04 01:57:53 +0530

    added updated Div.java

--- Div.java
+++ Div.java
@@ -1,7 +1,7 @@
 public class Div{
     public static void main(){
-        int a=5; 
+        int a=15; 
         int b=6;
-        System.out.println("this is diff"+(b-a));
+        System.out.println("this is diff"+(a-b));
     }
 }
