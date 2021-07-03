3ec40fff5a75bf5f32ca5f96ac021101af7e0802
faiyaz <misterfaiyaz123jio@gmail.com>
2021-07-04 01:43:06 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply{
+    public static void main(){
+        int a=5; 
+        int b=6;
+        System.out.println("this is product"+(a*b));
+    }
+}
--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(){
         int a=5; 
         int b=6;
-        System.out.println(a+b);
+        System.out.println("this is sum"+(a+b));
     }
 }
